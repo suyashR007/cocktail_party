c1ad8f721b2e253e28d21f23b70b41bd641d9cca
Suyash Rane <suyashrane9923@gmail.com>
2023-06-05 15:52:20 +0530

    Little better the before

--- lib/screens/drink_detail_screen.dart
+++ lib/screens/drink_detail_screen.dart
@@ -49,15 +49,20 @@ class _DrinkDetailsScreenState extends State<DrinkDetailsScreen> {
                 child: CachedNetworkImage(
                   imageUrl: widget.drink.strDrinkThumb!,
                   fit: BoxFit.cover,
-                  height: screenSize.height * 0.8,
+                  height: screenSize.height * 0.9,
                   width: double.infinity,
                   progressIndicatorBuilder: (context, url, progress) =>
                       const CircularProgressIndicator.adaptive(),
                 ),
               ),
             ),
-            SliverToBoxAdapter(
-              child: Padding(
+          ];
+        },
+        body: SingleChildScrollView(
+          child: Column(
+            // shrinkWrap: true,
+            children: [
+              Padding(
                 padding: const EdgeInsets.all(8.0),
                 child: Column(
                   mainAxisAlignment: MainAxisAlignment.start,
@@ -78,52 +83,17 @@ class _DrinkDetailsScreenState extends State<DrinkDetailsScreen> {
                       ),
                     ),
                     Text(
-                      widget.drink.strCategory!,
+                      "Category: ${widget.drink.strCategory!}",
                       style: GoogleFonts.albertSans(
                         fontWeight: FontWeight.w400,
                         fontSize: 16,
                       ),
                     ),
-                    Text(widget.drink.strGlass!),
-                    Consumer2<SaveProvider, MainScreenProvider>(
-                      builder:
-                          (context, saveProvider, mainScreenProvider, child) =>
-                              Center(
-                        child: FutureBuilder<bool>(
-                          future: saveProvider.checkList(drink: widget.drink),
-                          builder: (context, snapshot) {
-                            return SizedBox(
-                              width: screenSize.width * 0.6,
-                              child: ElevatedButton.icon(
-                                onPressed: () async {
-                                  if (snapshot.data!) {
-                                    Navigator.pop(context);
-                                    mainScreenProvider.changeIndex(2);
-                                  } else {
-                                    var resp = await saveProvider.saveItem(
-                                        drink: widget.drink);
-                                    ScaffoldMessenger.of(context).showSnackBar(
-                                        SnackBar(content: Text(resp)));
-                                  }
-                                },
-                                icon: (snapshot.hasData)
-                                    ? Icon((snapshot.data!)
-                                        ? Icons.favorite
-                                        : Icons.favorite_border)
-                                    : const Center(
-                                        child: CircularProgressIndicator(),
-                                      ),
-                                label: (snapshot.hasData)
-                                    ? Text((snapshot.data!)
-                                        ? 'Go to WishList'
-                                        : 'Add Drink to WIshList')
-                                    : const Center(
-                                        child: CircularProgressIndicator(),
-                                      ),
-                              ),
-                            );
-                          },
-                        ),
+                    Text(
+                      'Glass Type: ${widget.drink.strGlass!}',
+                      style: GoogleFonts.albertSans(
+                        fontWeight: FontWeight.w400,
+                        fontSize: 16,
                       ),
                     ),
                     SizedBox(height: screenSize.height * 0.01),
@@ -135,6 +105,7 @@ class _DrinkDetailsScreenState extends State<DrinkDetailsScreen> {
                       padding: const EdgeInsets.all(8),
                       child: Column(
                         crossAxisAlignment: CrossAxisAlignment.start,
+                        mainAxisSize: MainAxisSize.min,
                         children: [
                           Text(
                             'Instructions',
@@ -151,101 +122,143 @@ class _DrinkDetailsScreenState extends State<DrinkDetailsScreen> {
                   ],
                 ),
               ),
-            ),
-          ];
-        },
-        body: Container(
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(8),
-            color: Colors.purple[200]!.withOpacity(0.1),
-          ),
-          child: ListView(
-            padding: const EdgeInsets.all(8),
-            shrinkWrap: true,
-            children: [
-              Text(
-                'Ingredient',
-                style: GoogleFonts.albertSans(
-                  fontWeight: FontWeight.w600,
-                  fontSize: 16,
+              Container(
+                height: screenSize.height * 0.2,
+                margin: const EdgeInsets.all(8),
+                padding: const EdgeInsets.all(8),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(8),
+                  color: Colors.purple[200]!.withOpacity(0.1),
                 ),
-              ),
-              if (widget.drink.strIngredient1 != null)
-                IngredientView(
-                  ingradient: widget.drink.strIngredient1!,
-                  qualtity: widget.drink.strMeasure1!,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  // padding: const EdgeInsets.all(8),
+                  // shrinkWrap: true,
+                  children: [
+                    Text(
+                      'Ingredient',
+                      style: GoogleFonts.albertSans(
+                        fontWeight: FontWeight.w600,
+                        fontSize: 16,
+                      ),
+                    ),
+                    const Divider(thickness: 0.5),
+                    if (widget.drink.strIngredient1 != null)
+                      IngredientView(
+                        ingradient: widget.drink.strIngredient1!,
+                        qualtity: widget.drink.strMeasure1!,
+                      ),
+                    if (widget.drink.strIngredient2 != null)
+                      IngredientView(
+                        ingradient: widget.drink.strIngredient2!,
+                        qualtity: widget.drink.strMeasure2!,
+                      ),
+                    if (widget.drink.strIngredient3 != null)
+                      IngredientView(
+                        ingradient: widget.drink.strIngredient3!,
+                        qualtity: widget.drink.strMeasure3!,
+                      ),
+                    if (widget.drink.strIngredient4 != null)
+                      IngredientView(
+                        ingradient: widget.drink.strIngredient4!,
+                        qualtity: widget.drink.strMeasure4!,
+                      ),
+                    if (widget.drink.strIngredient5 != null)
+                      IngredientView(
+                        ingradient: widget.drink.strIngredient5!,
+                        qualtity: widget.drink.strMeasure5!,
+                      ),
+                    if (widget.drink.strIngredient6 != null)
+                      IngredientView(
+                        ingradient: widget.drink.strIngredient6!,
+                        qualtity: widget.drink.strMeasure6!,
+                      ),
+                    if (widget.drink.strIngredient7 != null)
+                      IngredientView(
+                        ingradient: widget.drink.strIngredient7!,
+                        qualtity: widget.drink.strMeasure7!,
+                      ),
+                    if (widget.drink.strIngredient8 != null)
+                      IngredientView(
+                        ingradient: widget.drink.strIngredient8!,
+                        qualtity: widget.drink.strMeasure8!,
+                      ),
+                    if (widget.drink.strIngredient9 != null)
+                      IngredientView(
+                        ingradient: widget.drink.strIngredient9,
+                        qualtity: widget.drink.strMeasure9!,
+                      ),
+                    if (widget.drink.strIngredient10 != null)
+                      IngredientView(
+                        ingradient: widget.drink.strIngredient10!,
+                        qualtity: widget.drink.strMeasure10!,
+                      ),
+                    if (widget.drink.strIngredient11 != null)
+                      IngredientView(
+                        ingradient: widget.drink.strIngredient11!,
+                        qualtity: widget.drink.strMeasure11!,
+                      ),
+                    if (widget.drink.strIngredient12 != null)
+                      IngredientView(
+                        ingradient: widget.drink.strIngredient12!,
+                        qualtity: widget.drink.strMeasure12!,
+                      ),
+                    if (widget.drink.strIngredient13 != null)
+                      IngredientView(
+                        ingradient: widget.drink.strIngredient13!,
+                        qualtity: widget.drink.strMeasure13!,
+                      ),
+                    if (widget.drink.strIngredient14 != null)
+                      IngredientView(
+                        ingradient: widget.drink.strIngredient14!,
+                        qualtity: widget.drink.strMeasure14!,
+                      ),
+                    if (widget.drink.strIngredient15 != null)
+                      IngredientView(
+                        ingradient: widget.drink.strIngredient15!,
+                        qualtity: widget.drink.strMeasure15!,
+                      ),
+                  ],
                 ),
-              if (widget.drink.strIngredient2 != null)
-                IngredientView(
-                  ingradient: widget.drink.strIngredient2!,
-                  qualtity: widget.drink.strMeasure2!,
-                ),
-              if (widget.drink.strIngredient3 != null)
-                IngredientView(
-                  ingradient: widget.drink.strIngredient3!,
-                  qualtity: widget.drink.strMeasure3!,
-                ),
-              if (widget.drink.strIngredient4 != null)
-                IngredientView(
-                  ingradient: widget.drink.strIngredient4!,
-                  qualtity: widget.drink.strMeasure4!,
-                ),
-              if (widget.drink.strIngredient5 != null)
-                IngredientView(
-                  ingradient: widget.drink.strIngredient5!,
-                  qualtity: widget.drink.strMeasure5!,
-                ),
-              if (widget.drink.strIngredient6 != null)
-                IngredientView(
-                  ingradient: widget.drink.strIngredient6!,
-                  qualtity: widget.drink.strMeasure6!,
-                ),
-              if (widget.drink.strIngredient7 != null)
-                IngredientView(
-                  ingradient: widget.drink.strIngredient7!,
-                  qualtity: widget.drink.strMeasure7!,
-                ),
-              if (widget.drink.strIngredient8 != null)
-                IngredientView(
-                  ingradient: widget.drink.strIngredient8!,
-                  qualtity: widget.drink.strMeasure8!,
-                ),
-              if (widget.drink.strIngredient9 != null)
-                IngredientView(
-                  ingradient: widget.drink.strIngredient9,
-                  qualtity: widget.drink.strMeasure9!,
-                ),
-              if (widget.drink.strIngredient10 != null)
-                IngredientView(
-                  ingradient: widget.drink.strIngredient10!,
-                  qualtity: widget.drink.strMeasure10!,
-                ),
-              if (widget.drink.strIngredient11 != null)
-                IngredientView(
-                  ingradient: widget.drink.strIngredient11!,
-                  qualtity: widget.drink.strMeasure11!,
-                ),
-              if (widget.drink.strIngredient12 != null)
-                IngredientView(
-                  ingradient: widget.drink.strIngredient12!,
-                  qualtity: widget.drink.strMeasure12!,
-                ),
-              if (widget.drink.strIngredient13 != null)
-                IngredientView(
-                  ingradient: widget.drink.strIngredient13!,
-                  qualtity: widget.drink.strMeasure13!,
-                ),
-              if (widget.drink.strIngredient14 != null)
-                IngredientView(
-                  ingradient: widget.drink.strIngredient14!,
-                  qualtity: widget.drink.strMeasure14!,
-                ),
-              if (widget.drink.strIngredient15 != null)
-                IngredientView(
-                  ingradient: widget.drink.strIngredient15!,
-                  qualtity: widget.drink.strMeasure15!,
-                ),
+              )
             ],
+          ),
+        ),
+      ),
+      bottomNavigationBar: Padding(
+        padding: const EdgeInsets.fromLTRB(0, 0, 0, 8),
+        child: Consumer2<SaveProvider, MainScreenProvider>(
+          builder: (context, saveProvider, mainScreenProvider, child) =>
+              FutureBuilder<bool>(
+            future: saveProvider.checkList(drink: widget.drink),
+            builder: (context, snapshot) {
+              return ElevatedButton.icon(
+                onPressed: () async {
+                  if (snapshot.data!) {
+                    Navigator.pop(context);
+                    mainScreenProvider.changeIndex(2);
+                  } else {
+                    var resp = await saveProvider.saveItem(drink: widget.drink);
+                    ScaffoldMessenger.of(context)
+                        .showSnackBar(SnackBar(content: Text(resp)));
+                  }
+                },
+                icon: (snapshot.hasData)
+                    ? Icon((snapshot.data!)
+                        ? Icons.favorite
+                        : Icons.favorite_border)
+                    : const Center(
+                        child: CircularProgressIndicator(),
+                      ),
+                label: (snapshot.hasData)
+                    ? Text((snapshot.data!)
+                        ? 'Go to WishList'
+                        : 'Add Drink to WIshList')
+                    : const Center(
+                        child: CircularProgressIndicator(),
+                      ),
+              );
+            },
           ),
         ),
       ),
